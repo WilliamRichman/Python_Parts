b50b70a10f20b2900d93c3b39e3b15e7b98ed714
William Richman <goat1120@gmail.com>
2022-08-09 12:40:05 -0400

    All about strings

---
+++ All_About_Strings.py
@@ -0,0 +1,28 @@
+#All about strings
+
+name = "william  test richman"
+firstN = "william"
+lastN = "richman"
+middleN = "test"
+fullN = firstN+ " " +middleN+ " " +lastN
+
+
+
+#This will make the first letter of each word caps .title 
+print(name.title())
+
+#to do lower and upper 
+print(name.lower())
+print(name.upper())
+
+#To combine parts of a string 
+print(fullN)
+
+#Adding white space
+# without white space 
+print("python")
+#with whitespace 
+print("\tpython")
+#You can strip white space with the \n\t command
+
+#NOTE if you have ' in your string like don't you will need to use double quotes ""
