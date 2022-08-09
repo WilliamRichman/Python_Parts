270a5392a9d8d0fe1db546c325f82fcbcb678154
William Richman <goat1120@gmail.com>
2022-08-09 14:59:42 -0400

    Added lists

---
+++ All about list.py
@@ -0,0 +1,19 @@
+#All about list and how to use them
+
+
+colors = ['red','green','blue','yellow','pink','light green']
+
+#Pint each item from the list by calling its index 
+print(colors[4])
+print(colors[2])
+
+
+#You can add methoods to this exp 
+print(colors[4].title())
+
+#you can ask for the last item in a list using the -1 
+print(colors[-1].title())
+
+#how to use this
+message = "My favorit color is " + colors[-1].title()+ "."
+print(message)
