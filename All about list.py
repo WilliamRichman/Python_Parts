cd2be7cd60cf35c2f70fbcea3cbf7aafa104bfe9
William Richman <goat1120@gmail.com>
2022-08-09 15:21:52 -0400

    Added some list info

--- All about list.py
+++ All about list.py
@@ -1,7 +1,6 @@
 #All about list and how to use them
-
-
 colors = ['red','green','blue','yellow','pink','light green']
+
 
 #Pint each item from the list by calling its index 
 print(colors[4])
@@ -17,3 +16,33 @@ print(colors[-1].title())
 #how to use this
 message = "My favorit color is " + colors[-1].title()+ "."
 print(message)
+
+
+#how to change the value of an item in a list
+anies = ['cat','dog','fish','bird']
+print(anies)
+
+#How to replace an element here i replace cat with lizard
+anies[0] = 'lizard'
+print(anies[0])
+
+#use the append command here I add chickes to the list
+anies.append('chickens')
+print(anies)
+
+#How to insert items into a list I give it the position of 1
+anies.insert(1,'bugs')
+print(anies)
+
+#how to remove elements from a list here I will delet bugs in position 1 
+del anies[1]
+print(anies)
+
+#The POP methood / POP allows you  to remove the last item in a list but it keeps teh value so you can still use it later 
+bikes = ['bmx','moutin','street']
+print(bikes)
+
+popped_bikes = bikes.pop()
+print(bikes)
+print(popped_bikes)
+
