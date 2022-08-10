d4758b8be0358fd790168fcd961af07fd06eb101
William Richman <goat1120@gmail.com>
2022-08-10 12:47:14 -0400

    updates before nap

--- Slicing_a_List.py
+++ Slicing_a_List.py
@@ -1,1 +1,12 @@
 #Slicing through lists 
+players = ["will","aika","daniel","joe","bob"]
+print(players[0:3])
+print(players[2:4])
+
+#For loop using slicing
+for player in players[:3]:
+    print(player.title())
+#This is short hand using a for loop to get the first 3 players of the list
+
+ 
+
