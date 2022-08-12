be4b1241bf75661b224f1f7e249b49df285bf24b
William Richman <goat1120@gmail.com>
2022-08-12 15:58:25 -0400

    Working in if statements up to boolons

--- If Statements.py
+++ If Statements.py
@@ -16,6 +16,56 @@ for fruit in fruits:
     else:
         print(fruit.title())
 
+#Conditional tests
+#this will come back true or false this is not true 
+car = 'Toyota'
+car == 'bmw'
+
+#this will also not be true due to the case 
+car == "toyota"
+#how to fix this 
+car.lower() == "toyota"
+
+#Checking for Inequality just add != This now comes back as True
+car != "Honda"
+
+#Compairing numbers 
+#You can include math comairisons <, <=, >, >=
+age = 16
+
+if age != 21:
+    print("They are to young to drink")
+
+answer = 45
+
+if answer != 50:
+    print("That Answer Is Incorreect Please Try Again")
+
+#Checking Multiple conditions 
+age_0 = 22
+age_1 = 18
+age_0 >= and age_1 >= 21
+#this comes back false
+
+#Using or to check conditions 
+age_0 >= or age_1 >= 25
+
+
+#Check to see if a value is in a list
+requested_toppings = ['cheese','olives','beef','chicken'] 
+
+'mushrooms' in requested_toppings
+
+'olives' in requested_toppings
+
+
+#Checking Weather a vaule is not in a list
+banned_users = ['billy','johny','daniel','aika','jorge']
+user = 'william'
+
+if user not in banned_users:
+    print(user.title() + ", you may play if you wish.")
+    
 
 
 
