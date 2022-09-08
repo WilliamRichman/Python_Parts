1cec342c374ce570ca9dd4108735fe1cbd75d841
William Richman <goat1120@gmail.com>
2022-09-08 12:44:45 -0400

    Push to fix errors on branches

---
+++ Dictionaries.py
@@ -0,0 +1,14 @@
+#Simple Dictionary
+allens = {'color' : 'green', 'points' : 5}
+print(allens['color'])
+print(allens['points'])
+
+
+#Adding new key value pairs 
+allens_0 = {'color':'red', 'points': 5}
+
+allens_0['x_position'] = 0
+allens_0['y_position'] = 25
+print(allens_0)
+
+
