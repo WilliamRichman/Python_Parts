43420913d68e651c948088db07ab07a774117a8c
William Richman <goat1120@gmail.com>
2022-09-08 13:15:51 -0400

    added a simple for loop page

---
+++ Types_for_loops.py
@@ -0,0 +1,14 @@
+#basic for loop construction
+fruits =['apple','grape','cherry']
+for fruit in fruits:
+    print(fruit)
+
+for x in fruits:
+    print(x)
+
+
+#to count to 100 with a for loop 
+for number in range(100):
+    print(number)
+
+
---
+++ list.ps1
@@ -0,0 +1,6 @@
+
+impier list = path C:"\users\Wrichman\Desktop\Lists"
+for User in users {
+    for eachsuer in suers $
+}
+print(.out)
