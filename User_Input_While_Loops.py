44bcdbd592010eaa6a81235d3838279bd6c104d4
William Richman <goat1120@gmail.com>
2022-10-04 13:48:28 -0400

    added new user input and have a print out message about cats and dogs

--- User_Input_While_Loops.py
+++ User_Input_While_Loops.py
@@ -1,10 +1,16 @@
 """ The input function waits for user input it looks like this input() """
 
-from email import message
-
-
-message = input("Tell how you feel? ")
+message = input("Please tell me your name. ")
 print(message)
 
-test = input("This is a test type something stupid! ")
+test = input("What kind of pet do you have a cat or a dog? ")
 print(test)
+
+""" How to capture data from the user  and insert a vaerble from the perivios question"""
+question = input("What is the color of your " + test + " ?")
+
+
+
+""" you can also add these all together like so """
+print( message + " I have a "  + test + " too I like them. I have never seen a " + question + " colored " + test + " before very cool. ")
+
